6b167ef1349f6b480c32d5d2dd6844fe2dd3d5c8
Alex Willmer <alex@moreati.org.uk>
2015-08-19 14:10:10 +0100

    Add setup() metadata for PyPI (except version) refs #8
    I've left version out for now, as a safety catch against doing the
    first release before we've decided on/fixed issue #8

--- setup.py
+++ setup.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
-from setuptools import setup
+import io
+import os
+
+from setuptools import setup, find_packages
+
+def read(fname, encoding='utf-8'):
+    path = os.path.join(os.path.dirname(__file__), fname)
+    with io.open(path, encoding=encoding) as f:
+        return f.read()
 
 setup(
     name='django-u2f',
     description="FIDO U2F security token support for Django",
+    long_description=read('README.rst'),
+    url='https://github.com/gavinwahl/django-u2f',
+
+    packages=find_packages(exclude=['testproj']),
+    include_package_data=True,
+
     install_requires=[
         'django-argonauts',
         'python-u2flib-server',
@@ -17,6 +31,9 @@ setup(
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Security',
         'Topic :: Security :: Cryptography',
     ],
