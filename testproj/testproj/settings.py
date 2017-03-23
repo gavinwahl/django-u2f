ff29191ac3b50f252570db74cd6d8eb14f5094bf
Gavin Wahl <gavinwahl@gmail.com>
2017-03-22 19:12:46 -0600

    Support Django 1.10
    Fixes #20

--- testproj/testproj/settings.py
+++ testproj/testproj/settings.py
@@ -24,8 +24,7 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -88,6 +87,23 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'u2f:two-factor-settings'
 LOGIN_URL = 'u2f:login'
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates'),
-)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- testproj/testproj/urls.py
+++ testproj/testproj/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+
+import django_u2f.urls
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^u2f/', include('django_u2f.urls', 'u2f')),
+    url(r'^u2f/', include(django_u2f.urls, namespace='u2f')),
     url(r'^admin/', include(admin.site.urls)),
 ]
