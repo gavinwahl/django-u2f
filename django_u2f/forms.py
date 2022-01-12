ba6515ebfe21b282081eb2eec831f777bc2018a4
Brian Perrett <bperrett@fusionbox.com>
2022-01-11 18:57:46 -0700

    Authenticate using webauthn.

--- django_u2f/forms.py
+++ django_u2f/forms.py
@@ -3,6 +3,10 @@ import json
 from django import forms
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+import webauthn
+from webauthn import options_to_json, base64url_to_bytes
+from webauthn.helpers.structs import PublicKeyCredentialDescriptor, AuthenticationCredential
+from base64 import b64decode
 
 from u2flib_server import u2f
 
@@ -23,19 +27,47 @@ class KeyResponseForm(SecondFactorForm):
         if self.data:
             self.sign_request = self.request.session['u2f_sign_request']
         else:
-            self.sign_request = u2f.begin_authentication(self.appId, [
-                d.to_json() for d in self.user.u2f_keys.all()
-            ])
-            self.request.session['u2f_sign_request'] = self.sign_request
+            options = webauthn.generate_authentication_options(
+                rp_id='localhost',
+                allow_credentials=[
+                    PublicKeyCredentialDescriptor(id=base64url_to_bytes(x.key_handle))
+                    for x in self.user.u2f_keys.all()
+                ]
+            )
+            options = options_to_json(options)
+            options = json.loads(options)
+
+            # should appid come from the u2f_key model?
+            options['extensions'] = {
+                'appid': 'https://localhost:8000'
+            }
+            options = {'publicKey': options}
+            self.sign_request = options
+            self.request.session['u2f_sign_request'] = options
 
     def validate_second_factor(self):
-        response = json.loads(self.cleaned_data['response'])
+        response = self.cleaned_data['response']
         try:
-            device, login_counter, _ = u2f.complete_authentication(self.sign_request, response)
+            data = self.request.session['u2f_sign_request']['publicKey']
+            json_data = json.loads(response)
+            key = self.user.u2f_keys.get(key_handle=json_data['id'])
+            expected_rp_id = data['rpId']
+            # use app id as expected_rp_id if appid extension is provided
+            # https://github.com/duo-labs/py_webauthn/issues/116#issuecomment-1010385763
+            if json_data['clientExtensionResults'].get('appid', False):
+                expected_rp_id = key.app_id
+            verification = webauthn.verify_authentication_response(
+                credential=AuthenticationCredential.parse_raw(response),
+                expected_challenge=base64url_to_bytes(data['challenge']),
+                expected_rp_id=expected_rp_id,
+                expected_origin='https://localhost:8000',
+                credential_public_key=base64url_to_bytes(key.public_key),
+                credential_current_sign_count=0,
+                require_user_verification=False,
+            )
             # TODO: store login_counter and verify it's increasing
-            device = self.user.u2f_keys.get(key_handle=device['keyHandle'])
-            device.last_used_at = timezone.now()
-            device.save()
+            key.last_used_at = timezone.now()
+            key.save()
             del self.request.session['u2f_sign_request']
             return True
         except ValueError:
--- django_u2f/static/django_u2f/django_u2f.js
+++ django_u2f/static/django_u2f/django_u2f.js
@@ -1,38 +1,45 @@
-function handleU2FError(resp) {
-  if (resp.errorCode) {
-    document.getElementById('u2f-status').textContent = 'Error with U2F';
-    return true;
-  } else {
-    return false;
-  }
-}
-
 if (typeof window.u2f === 'undefined')
   document.getElementById('u2f-not-defined-error').style.display = 'block';
 
-
-var request;
-var requestElem = document.getElementById('django_u2f_request');
-if (requestElem) {
-  request = JSON.parse(requestElem.textContent);
-  u2f.sign(request.appId, request.challenge, request.registeredKeys, function(resp) {
-    var form = document.getElementById('u2f-form');
-    form.response.value = JSON.stringify(resp);
-    if (!handleU2FError(resp)) {
-      form.submit();
-    }
-  });
+function ab2str(buf) {
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(buf))).replace(/\//g, '_').replace(/\+/g, '-').replace(/=*$/, '');
+}
+function str2ab(enc) {
+  var str = atob(enc.replace(/_/g, '/').replace(/-/g, '+'));
+  var buf = new ArrayBuffer(str.length);
+  var bufView = new Uint8Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
+function processCredentials(credentials) {
+  // modifies the object
+  // converts specific fields to ArrayBuffers of Uint8Array
+  credentials.publicKey.challenge = str2ab(credentials.publicKey.challenge)
+  for (var i=0; i<credentials.publicKey.allowCredentials.length; i++) {
+    credentials.publicKey.allowCredentials[i].id = str2ab(credentials.publicKey.allowCredentials[i].id)
+  }
 }
 
-
-var registrationElem = document.getElementById('django_u2f_registration');
-if (registrationElem) {
-  request = JSON.parse(registrationElem.textContent);
-  u2f.register(request.appId, request.registerRequests, request.registeredKeys, function(resp) {
-    var form = document.getElementById('u2f-form');
-    form.response.value = JSON.stringify(resp);
-    if (!handleU2FError(resp)) {
-      form.submit();
-    }
-  });
+async function get_credentials() {
+  let cred = JSON.parse(document.getElementById('django_u2f_request').innerHTML);
+  processCredentials(cred);
+  const resp = await navigator.credentials.get(cred)
+  const respObject = {
+    id: resp.id,
+    rawId: ab2str(resp.rawId),
+    response: {
+      authenticatorData: ab2str(resp.response.authenticatorData),
+      clientDataJSON: ab2str(resp.response.clientDataJSON),
+      signature: ab2str(resp.response.signature),
+      userHandle: resp.response.userHandle,
+    },
+    type: resp.type,
+    clientExtensionResults: resp.getClientExtensionResults(),
+  }
+  const form = document.getElementById('u2f-form')
+  form.response.value = JSON.stringify(respObject)
+  form.submit()
 }
+get_credentials()
