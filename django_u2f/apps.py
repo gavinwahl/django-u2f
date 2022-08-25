6921e6043cd80ee1e9154e85f9f65e6ecbd32afa
bgaudino <bgaudino@gmail.com>
2022-08-25 15:36:49 -0500

    Prevent additional migrations when using Django >= 3.2

--- django_u2f/apps.py
+++ django_u2f/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class U2FConfig(AppConfig):
     name = 'django_u2f'
+    default_auto_field = 'django.db.models.AutoField'
 
     def monkeypatch_login_view(self):
         from .admin import monkeypatch_admin
